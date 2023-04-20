1549c650d5e98419e2f97eaafb8cc0df67951ff6
raw1248 <raweewan.mhtech@gmail.com>
2023-04-20 16:28:54 +0700

    add sample audio

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,17 @@ import React, { useRef, useState } from "react";
 import axios from "axios";
 
 const soundPath = "/PMR_5_v2.mp3";
+const samplePath = "/sample_audio.mp3";
 // const soundPath = "/freesound.mp3";
 const palettes = ["black", "#323E45", "#3A4E51", "#5A786F", "#8BA88E", "#CCD2C6", "white"];
 
 export default function Home() {
   const [play, setPlay] = useState(false);
   const [btnText, setBtnText] = useState("Play");
+  const [samplePlay, setSamplePlay] = useState(false);
+  const [sampleBtnText, setSampleBtnText] = useState("PLAY SAMPLE");
   const soundRef = useRef(null);
+  const sampleRef = useRef(null);
 
   function markTime() {
     axios.get("https://script.google.com/macros/s/AKfycbwN8HU26QZekCC6M0riOwJEotnDWhrieyVQvEj4DIpw8KtID7H-vHkiva_FCCKSlozm/exec")
@@ -42,8 +46,25 @@ export default function Home() {
     }
   }
 
+  function manageSample() {
+    console.log('currentTime :>> ', sampleRef.current?.currentTime);
+    if (samplePlay) {
+      sampleRef.current?.pause();
+      setSamplePlay(false);
+      setSampleBtnText("PLAY SAMPLE");
+    } else {
+      sampleRef.current?.play();
+      setSamplePlay(true);
+      setSampleBtnText("PAUSE SAMPLE");
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{ backgroundColor: palettes[5], color: palettes[0] }}>
+      <button onClick={manageSample} className="rounded-full w-48 p-2 absolute top-5 left-5" style={{ backgroundColor: palettes[4], color: palettes[5] }}>
+        {sampleBtnText}
+      </button>
+      <audio ref={sampleRef} loop src={samplePath} />
       <button
         onClick={manageAudio}
         type="button"
